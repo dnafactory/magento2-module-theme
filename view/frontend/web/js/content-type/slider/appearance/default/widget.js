0ebbb068476cc89a6f3cd8300c557ee0bd7f809e
Antonio Vitale <antonio.vitale@dnafactory.it>
2021-04-23 18:13:11 +0200

    Unreleased
    + Added product content-type override for tiny slider
    + Added slider content-type override for tiny slider
    + Override slider content-type style to use tiny slider

---
+++ view/frontend/web/js/content-type/slider/appearance/default/widget.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'Magento_PageBuilder/js/events',
+    'underscore',
+    'dnaCarousel',
+    'dna.responsive',
+], function ($, events, _,dnaCarousel, dnaResponsive) {
+    'use strict';
+
+    /**
+     * Initialize slider.
+     *
+     * @param {jQuery} $carouselElement
+     * @param {Object} config
+     */
+    function buildDnaCarousel($carouselElement, config) {
+        dnaCarousel(config, $carouselElement[0]);
+    }
+
+    return function (config, sliderElement) {
+        var $element = $(sliderElement),
+            sliderConfig = {
+                autoplay: $element.data('autoplay'),
+                autoplayTimeout: $element.data('autoplay-speed') || 0,
+                fade: $element.data('fade'),
+                items: 1,
+                infinite: $element.data('infinite-loop'),
+                nav: $element.data('show-arrows'),
+                dots: $element.data('show-dots'),
+                responsive: false
+            };
+
+        buildDnaCarousel($element, sliderConfig);
+
+        // Redraw slide after content type gets redrawn
+        events.on('contentType:redrawAfter', function (args) {
+            if ($element.closest(args.element).length) {
+                $element.rebuildInstance();
+            }
+        });
+    };
+});
